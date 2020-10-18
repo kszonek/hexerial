01832612e8f4dabc2d7ca8a40339334ea6fe336e
kszonek <k@kszonek.pl>
2020-10-18 10:04:59 +0200

    Help dialog printing added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
+#include <iostream>
+
 #include "config.hpp"
 #include "hexerial.hpp"
+#include "version.hpp"
+
+void print_help(void)
+{
+    std::cout << "Hexesrial " << VERSION << std::endl;
+    std::cout << "Usage: " << std::endl;
+    std::cout << "\t-b [value]\t"
+              << "set baudrate (default: 9600)" << std::endl;
+    std::cout << "\t-h\t\t"
+              << "show this help dialog" << std::endl;
+    std::cout << "\t-p [value]\t"
+              << "set serial port path (default: /dev/ttyUSB0)" << std::endl;
+    std::cout << "\t-t\t\t"
+              << "print timestamp of each msg" << std::endl;
+    std::cout << "\t-v\t\t"
+              << "verbose mode" << std::endl;
+    exit(0);
+}
 
 int main(int argc, char *argv[])
 {
     Config cfg(argc, argv);
+
+    if (Config::cmdOptionExists("-h"))
+        print_help();
 
     Hexerial hexerial;
     hexerial.run();
